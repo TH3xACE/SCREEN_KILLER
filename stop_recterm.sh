f95ae99badab6ac0c7606002f967cbc1f6c47293
David B <44925796+TH3xACE@users.noreply.github.com>
2020-02-18 15:51:53 +0100

    Update stop_recterm.sh

--- stop_recterm.sh
+++ stop_recterm.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+##### (Cosmetic) Colour output
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+BLUE="\033[01;34m"     # Heading
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
+
 trm=$(cat $HOME/.bashrc | grep -v "tcommands.txt")
 #echo "$trm"
 echo "$trm" > $HOME/.bashrc
